8032f3e713b5d4054c0a051bb2dad51d66c72690
Anton <64197715+yushkevich-a-a@users.noreply.github.com>
2024-05-22 20:34:51 +0300

    Update script.sh

--- script.sh
+++ script.sh
@@ -4,5 +4,5 @@ mkdir attachment
 
 for (( counter = 1; counter <= 10; counter++ ))
 do 
-curl https://picsum.photos/800/400 -L > ./attachment/"image_$counter"
+curl https://picsum.photos/800/400 -L > ./attachment/"image_$counter".jpg
 done
